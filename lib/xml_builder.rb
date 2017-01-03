a667f788e77cc09fd93a073b7fd45a2f6c39a6ae
Hanan Fligel <hanan@goroomer.com>
2017-01-03 13:06:05 +0200

    Fixed a bug

--- lib/xml_builder.rb
+++ lib/xml_builder.rb
@@ -22,11 +22,11 @@ class XmlBuilder
     details << registration_mode
 
     first_name = Ox::Element.new('firstName')
-    first_name << @args[:user_first_name]
+    first_name << @args[:user_first_name].to_s.gsub('-', ' ')
     details << first_name
 
     last_name = Ox::Element.new('lastName')
-    last_name << @args[:user_last_name]
+    last_name << @args[:user_last_name].to_s.gsub('-', ' ')
     details << last_name
 
     address = Ox::Element.new('address1')
